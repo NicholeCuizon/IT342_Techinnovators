710b214a2efa69619417e8f60993d3c8be84f4a2
NicholeCuizon <166021519+NicholeCuizon@users.noreply.github.com>
2024-05-15 18:21:21 +0800

    login update in views.py

--- pizza_alley_app/views.py
+++ pizza_alley_app/views.py
@@ -6,3 +6,30 @@ from django.template import loader
 def landing(request):
     template = loader.get_template('landing.html')
     return HttpResponse(template.render())
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        # Check if the user exists in the database
+        user = Accounts.objects.filter(email=email, password=password).first()
+        if user:
+            # Password is correct, log in the user
+            request.session['user_email'] = email
+
+            # Get the user's information
+            user_info = {
+                'firstname': user.firstname
+            }
+
+            if user.user_type == 1:
+                return render(request, 'dashboard_admin.html', {'user_info': user_info})
+            elif user.user_type == 2:
+                return render(request, 'dashboard_customer.html', {'user_info': user_info})
+            else:
+                return render(request, 'dashboard_employee.html', {'user_info': user_info})
+
+        return render(request, 'login.html', {'login_failed': True})
+
+    return render(request, 'login.html')
