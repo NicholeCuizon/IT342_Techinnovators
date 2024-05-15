40465858b5dae08d1809deaeb8234ffee0812b07
NicholeCuizon <166021519+NicholeCuizon@users.noreply.github.com>
2024-05-15 18:24:48 +0800

    login and dashboard views

--- pizza_alley_app/urls.py
+++ pizza_alley_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('login/', views.login, name='login'),
+    path('dashboard_admin/', views.dashboard_admin, name='dashboard_admin'),
+    path('dashboard_employee/', views.dashboard_employee, name='dashboard_employee'),
+    path('dashboard_customer/', views.dashboard_customer, name='dashboard_customer'),
 ]
